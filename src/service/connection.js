2e85f91a691d731126178b9f4df351fa17239b23
yellowYiFeng <827029595@qq.com>
2018-10-08 15:55:52 +0800

    Update connection.js

--- src/service/connection.js
+++ src/service/connection.js
@@ -1,6 +1,4 @@
-const HOST = "http://192.168.0.100:83/app/api"; //测试
-// const HOST = "http://apppreview.huiby.net/app/api"; //预发
-// const HOST = "https://wx.huiby.net/api"; //产线
+const HOST = ""; //测试
 
 export {
     HOST
